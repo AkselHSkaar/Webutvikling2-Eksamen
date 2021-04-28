419f4346cbe7daaca842a41d69cd79c9a096260b
Aksel <akselhskaar@hotmail.no>
2021-04-28 17:12:51 +0200

    added bookingService.js with get and post

---
+++ find-musician-vue/src/services/bookingService.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { reactive, toRefs } from 'vue'
+
+export default function bookingService() {
+
+    const booking = reactive({ bookingList: [] });
+
+    const getBookings = () => {
+        axios("https://localhost:5001/booking")
+            .then( response=> {
+                booking.bookingList = response.data;
+            } )
+    }
+
+    const createNewBooking = ( postBooking ) => {
+        axios.post("https://localhost:5001/booking/", postBooking)
+            .then( response => {
+                booking.bookingList.push( response.data );
+            } )
+    }
+
+    return {
+        ...toRefs(booking),
+        getBookings,
+        createNewBooking
+    }
+}

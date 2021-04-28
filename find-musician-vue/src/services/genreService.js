824401d52d368e4eb2eccae52cef413f3b40fb90
Aksel <akselhskaar@hotmail.no>
2021-04-28 17:29:43 +0200

    added genreService with get-method

---
+++ find-musician-vue/src/services/genreService.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { reactive, toRefs } from 'vue'
+
+export default function genreService() {
+
+    const genre = reactive({ genreList: [] });
+
+    const getGenres = () => {
+        axios("https://localhost:5001/genre")
+            .then( response => {
+                genre.genreList = response.data;
+            } )
+    }
+
+    return {
+        ...toRefs(genre),
+        getGenres
+    }
+}

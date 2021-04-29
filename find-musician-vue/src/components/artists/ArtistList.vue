4bca765547480738a22886817b188946330d4bb6
Johantschudi <johan@johanfelix.no>
2021-04-29 13:15:53 +0200

    Update ArtistList.vue

--- find-musician-vue/src/components/artists/ArtistList.vue
+++ find-musician-vue/src/components/artists/ArtistList.vue
@@ -5,17 +5,20 @@
             <label for="floatingInput">Søk etter artist</label>
         </div>
         <div class="row g-4">
-            <div class="col-12 col-sm-6 col-lg-4 col-xl-3" v-for="( artist, i ) in artistList" :key="i">
-                <artist-item
-                    :name="artist.name"
-                    :genre="artist.genre"
-                    :price="artist.price"
-                    :instrument="artist.instrument"
-                    :biography="artist.biography"
-                    :rating="artist.rating"
-                    :image="artist.image"
-                ></artist-item>
+            <div v-if="inputValue != ''">
+                <div class="col-12 col-sm-6 col-lg-4 col-xl-3" v-for="( artist, i ) in artistList" :key="i">
+                    <artist-item
+                        :name="artist.name"
+                        :genre="artist.genre"
+                        :price="artist.price"
+                        :instrument="artist.instrument"
+                        :biography="artist.biography"
+                        :rating="artist.rating"
+                        :image="artist.image"
+                    ></artist-item>
+                </div>
             </div>
+            
         </div>
         
     </section>

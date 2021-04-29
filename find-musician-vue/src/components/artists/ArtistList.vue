6ce679782509f48d975e5268b375b2ef2b9fc339
Johantschudi <johan@johanfelix.no>
2021-04-29 13:25:51 +0200

    realtime filter artistList

--- find-musician-vue/src/components/artists/ArtistList.vue
+++ find-musician-vue/src/components/artists/ArtistList.vue
@@ -1,11 +1,30 @@
 <template>
     <section>
-        <div class="form-floating mb-3">
-            <input v-model="inputValue" @keyup="listArtistsByName" type="text" class="form-control" id="floatingInput" placeholder="Ole ivars">
-            <label for="floatingInput">Søk etter artist</label>
+        <div class="row">
+            <div class="col form-floating mb-3">
+                <input v-model="inputValue" @keyup="listArtistsByName" type="text" class="form-control" id="floatingInput" placeholder="Ole ivars">
+                <label for="floatingInput">Søk etter artist</label>
+            </div>
+            <div class="col">
+
+            </div>
         </div>
-        <div class="row g-4">
-            <div v-if="inputValue != ''">
+        
+        <div>
+            <div v-if="inputValue != ''" class="row g-4">
+                <div class="col-12 col-sm-6 col-lg-4 col-xl-3" v-for="( artist, i ) in searchResult" :key="i">
+                    <artist-item
+                        :name="artist.name"
+                        :genre="artist.genre"
+                        :price="artist.price"
+                        :instrument="artist.instrument"
+                        :biography="artist.biography"
+                        :rating="artist.rating"
+                        :image="artist.image"
+                    ></artist-item>
+                </div>
+            </div>
+            <div v-else class="row g-4">
                 <div class="col-12 col-sm-6 col-lg-4 col-xl-3" v-for="( artist, i ) in artistList" :key="i">
                     <artist-item
                         :name="artist.name"
@@ -49,6 +68,7 @@ export default {
             artistList,
             inputValue,
             listArtistsByName,
+            searchResult
         }
     }
 }

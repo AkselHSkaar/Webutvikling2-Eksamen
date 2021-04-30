ef9caf82bba68dcd2b939181f39211e8b975e04a
Aksel <akselhskaar@hotmail.no>
2021-04-30 15:52:06 +0200

    fixed datatype + new db migration

--- FindMusicianApi/Migrations/20210430135133_InitialCreate2.Designer.cs
+++ FindMusicianApi/Migrations/20210430135133_InitialCreate2.Designer.cs
@@ -2,14 +2,16 @@
 using FindMusicianApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FindMusicianApi.Migrations
 {
     [DbContext(typeof(FindMusicianContext))]
-    partial class FindMusicianContextModelSnapshot : ModelSnapshot
+    [Migration("20210430135133_InitialCreate2")]
+    partial class InitialCreate2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

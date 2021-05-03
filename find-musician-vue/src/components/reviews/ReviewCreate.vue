e519c55c551b50cd21aa37883059b46e6492a610
Johantschudi <johan@johanfelix.no>
2021-05-03 18:49:40 +0200

    Masse drit

--- find-musician-vue/src/components/reviews/ReviewCreate.vue
+++ find-musician-vue/src/components/reviews/ReviewCreate.vue
@@ -1,6 +1,5 @@
 <template>
     <section>
-        <h3>Legg til en anmeldelse</h3>
         <div>
             <form>
                 <div class="form-floating mb-3">
@@ -18,8 +17,37 @@
                     <label for="text-input">Anmeldelse:</label>
                 </div>
                 <div class="form-floating mb-3">
-                    <input v-model="stars" type="text" id="stars-input" class="form-control" placeholder="Navn">
-                    <label for="stars-input">Stjerner (1-5)</label>
+                    <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                        <input v-model="stars" type="radio" class="btn-check" name="starsradio" id="btnradio1" value="1" autocomplete="off" checked>
+                        <label class="btn btn-outline-primary" for="btnradio1">
+                            <img v-if="parseInt(newReview.stars) > 0" class="star" :src="require('@/assets/star.png')">
+                            <img v-else class="star" :src="require('@/assets/star-gray.png')">
+                        </label>
+
+                        <input v-model="stars" type="radio" class="btn-check" name="starsradio" id="btnradio2" value="2" autocomplete="off">
+                        <label class="btn btn-outline-primary" for="btnradio2">
+                            <img v-if="parseInt(newReview.stars) >= 2 && parseInt(newReview.stars) > 0" class="star" :src="require('@/assets/star.png')">
+                            <img v-else class="star" :src="require('@/assets/star-gray.png')">
+                        </label>
+
+                        <input v-model="stars" type="radio" class="btn-check" name="starsradio" id="btnradio3" value="3" autocomplete="off">
+                        <label class="btn btn-outline-primary" for="btnradio3">
+                            <img v-if="parseInt(newReview.stars) >= 3 && parseInt(newReview.stars) > 0" class="star" :src="require('@/assets/star.png')">
+                            <img v-else class="star" :src="require('@/assets/star-gray.png')">
+                        </label>
+
+                        <input v-model="stars" type="radio" class="btn-check" name="starsradio" id="btnradio4" value="4" autocomplete="off">
+                        <label class="btn btn-outline-primary" for="btnradio4">
+                            <img v-if="parseInt(newReview.stars) >= 4 && parseInt(newReview.stars) > 0" class="star" :src="require('@/assets/star.png')">
+                            <img v-else class="star" :src="require('@/assets/star-gray.png')">
+                        </label>
+
+                        <input v-model="stars" type="radio" class="btn-check" name="starsradio" id="btnradio5" value="5" autocomplete="off">
+                        <label class="btn btn-outline-primary" for="btnradio5">
+                            <img v-if="parseInt(newReview.stars) == 5" class="star" :src="require('@/assets/star.png')">
+                            <img v-else class="star" :src="require('@/assets/star-gray.png')">
+                        </label>
+                    </div>
                 </div>
                 <div>
                     <input @click="addReview" type="button" value="Legg til anmeldelse" class="form-control bg-success text-white mt-2">
@@ -40,7 +68,9 @@ export default {
         
         const { artistList, getArtists } = artistService();
         const { createNewReview } = reviewService();
-        const newReview = reactive({ stars: "", title: "", text: "", artist: "" });
+        const newReview = reactive({ stars: "3", title: "", text: "", artist: "" });
+
+        //const consoleLog = () => { console.log(newReview.stars) };
 
         getArtists();
 
@@ -52,15 +82,21 @@ export default {
                 artist: newReview.artist
             }
 
-            createNewReview( postReview );
-            location.reload();
+            createNewReview( postReview ).then(() => location.reload());
         }
 
         return {
             artistList,
             ...toRefs( newReview ),
-            addReview
+            addReview,
+            newReview
         }
     }
 }
 </script>
+
+<style scoped>
+    .star{
+        height: 25px;
+    }
+</style>

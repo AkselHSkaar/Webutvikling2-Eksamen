ecbd18678d83a8083c31f145b164637c9b633ab9
Aksel <akselhskaar@hotmail.no>
2021-05-03 16:13:15 +0200

    reviewList

---
+++ find-musician-vue/src/components/reviews/ReviewList.vue
@@ -0,0 +1,35 @@
+<template>
+    <section>
+        <h3>Alle omtaler:</h3>
+        <review-item v-for="( review, i ) in reviewList" :key="i"
+        :id="review.id"
+        :stars="review.stars"
+        :title="review.title"
+        :text="review.text"
+        :artist="review.artist"
+        ></review-item>
+    </section>
+</template>
+
+<script>
+import reviewService from '../../services/reviewService'
+import ReviewItem from './ReviewItem'
+
+export default {
+    name: 'ReviewList',
+    components: {
+        ReviewItem
+    },
+    setup() {
+        
+        const { reviewList, getReviews } = reviewService();
+
+        getReviews();
+
+        return {
+            reviewList
+        }
+
+    }
+}
+</script>

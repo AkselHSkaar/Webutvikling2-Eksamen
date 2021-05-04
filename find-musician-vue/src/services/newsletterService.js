0cd64a8d02e2dfbb4645433661335501235a7fa8
Aksel <akselhskaar@hotmail.no>
2021-05-04 15:38:46 +0200

    newsletterService

---
+++ find-musician-vue/src/services/newsletterService.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { reactive, toRefs } from 'vue'
+
+export default function newsletterService() {
+
+    const newsletter = reactive({ newsletterList: [] });
+
+    const getNewsletter = () => {
+        axios("https://localhost:5001/newsletter")
+            .then( response => {
+                newsletter.newsletterList = response.data;
+            } )
+    }
+
+    const createNew = () => {
+        axios.post("https://localhost:5001/newsletter", postEmail)
+            .then( response => {
+                newsletter.newsletterList.push(response.data);
+            } )
+    }
+
+    return {
+        ...toRefs(newsletter),
+        getNewsletter,
+        createNew
+    }
+}

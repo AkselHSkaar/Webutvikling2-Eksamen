03fc8cf6ea46db6b19e2e162f9ef4f8a86978573
Johantschudi <johan@johanfelix.no>
2021-05-05 18:43:31 +0200

    color coding artists and oppdrag by genre

--- find-musician-vue/src/services/genreService.js
+++ find-musician-vue/src/services/genreService.js
@@ -3,7 +3,7 @@ import { reactive, toRefs } from 'vue'
 
 export default function genreService() {
 
-    const genre = reactive({ genreList: [] });
+    const genre = reactive({ genreList: [], genreByName: {} });
 
     const getGenres = () => {
         return axios("https://localhost:5001/genre")
@@ -12,8 +12,16 @@ export default function genreService() {
             } )
     }
 
+    const getGenreByName = (genreName) => {
+        return axios(`https://localhost:5001/genre/getbyname/${genreName}`)
+            .then( response => {
+                genre.genreByName = response.data;
+            } )
+    }
+
     return {
         ...toRefs(genre),
-        getGenres
+        getGenres,
+        getGenreByName
     }
 }
